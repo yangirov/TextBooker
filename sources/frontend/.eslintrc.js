81b4c6b30e2246880e136f129a74b8438e82a722
Emil Yangirov <emil.yangirov@list.ru>
2021-05-17 19:48:55 +0500

    chore(front): Tune ESLint and Prettier rules

--- sources/frontend/.eslintrc.js
+++ sources/frontend/.eslintrc.js
@@ -3,14 +3,39 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-  ],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: [2, 'never'],
+    quotes: [2, 'single', { avoidEscape: true }],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        tabWidth: 2,
+        bracketSpacing: true,
+        vueIndentScriptAndStyle: false
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
--- sources/frontend/src/views/Editor.vue
+++ sources/frontend/src/views/Editor.vue
@@ -1,39 +1,52 @@
 <template>
   <el-tabs type="border-card">
     <el-tab-pane label="Start" class="full-wh">
-      <span slot="label"><i class="el-icon-s-home"></i> {{ $t('tabs.start.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-s-home"></i> {{ $t('tabs.start.name') }}</span
+      >
       <StartWork />
     </el-tab-pane>
 
     <el-tab-pane label="Config" v-if="isAuth" class="full-wh">
-      <span slot="label"><i class="el-icon-picture"></i> {{ $t('tabs.template.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-picture"></i> {{ $t('tabs.template.name') }}</span
+      >
       <TemplateGallery />
     </el-tab-pane>
 
     <el-tab-pane label="Settings" v-if="isAuth" class="full-wh">
-      <span slot="label"><i class="el-icon-setting"></i> {{ $t('tabs.settings.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-setting"></i> {{ $t('tabs.settings.name') }}</span
+      >
       {{ $t('common.empty') }}
     </el-tab-pane>
 
     <el-tab-pane label="Pages" v-if="isAuth" class="full-wh">
-      <span slot="label"><i class="el-icon-tickets"></i> {{ $t('tabs.pages.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-tickets"></i> {{ $t('tabs.pages.name') }}</span
+      >
       {{ $t('common.empty') }}
     </el-tab-pane>
 
     <el-tab-pane label="Blocks" v-if="isAuth" class="full-wh">
-      <span slot="label"><i class="el-icon-document-copy"></i> {{ $t('tabs.blocks.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-document-copy"></i>
+        {{ $t('tabs.blocks.name') }}</span
+      >
       <Blocks />
     </el-tab-pane>
 
     <el-tab-pane label="Publish" v-if="isAuth" class="full-wh">
-      <span slot="label"><i class="el-icon-monitor"></i> {{ $t('tabs.publish.name') }}</span>
+      <span slot="label"
+        ><i class="el-icon-monitor"></i> {{ $t('tabs.publish.name') }}</span
+      >
       {{ $t('common.empty') }}
     </el-tab-pane>
   </el-tabs>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from 'vuex'
 import StartWork from '@/components/Editor/StartWork/StartWork.vue'
 import TemplateGallery from '@/components/Editor/Templates/TemplateGallery.vue'
 import Blocks from '@/components/Editor/Blocks/Blocks.vue'
