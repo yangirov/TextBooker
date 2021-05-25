7d48f8745c94c1cdfdc439c9a83ee145cc4e3e6c
Emil Yangirov <emil.yangirov@list.ru>
2021-05-25 19:13:16 +0500

    feat(front): Add About and Feedback pages

---
+++ sources/frontend/src/views/About.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="row">
+    <div class="inner">
+      <div class="columns">
+        <div class="column is-full">
+          <h1 class="title" v-html="$t('about.title')"></h1>
+
+          <div class="buttons mt-2">
+            <router-link class="button is-blue is-medium" to="editor">
+              {{ $t('about.getStarted') }}
+            </router-link>
+
+            <a
+              class="button is-light is-medium"
+              href="help"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              {{ $t('common.help') }}
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns mt-3">
+        <div class="column is-half">
+          <ul>
+            <li v-for="item in $t('about.vantages')" :key="item.title">
+              <h2 class="subtitle">{{ item.title }}</h2>
+              <p>{{ item.desc }}</p>
+            </li>
+          </ul>
+        </div>
+
+        <div class="column is-half">
+          <iframe
+            class="iframe"
+            src="https://www.youtube.com/embed/ezp60FUnjGg"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass">
+.row
+  height: 70%
+  display: flex
+  align-items: center
+  justify-content: center
+
+.inner
+  display: flex
+  flex-direction: column
+  align-items: flex-start
+  justify-content: center
+
+.iframe
+  width: 100%
+  height: 100%
+  border: 1px solid #ccc
+  margin-left: 2em
+
+.subtitle
+  padding-top: 1em
+  margin-bottom: .5em !important
+</style>
---
+++ sources/frontend/src/views/Feedback.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="columns is-mobile is-centered">
+    <div class="column is-half">
+      <h1 class="title mb-2">{{ $t('feedback.title') }}</h1>
+      <FeedbackForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import FeedbackForm from '@/components/Information/FeedbackForm.vue'
+
+export default {
+  components: {
+    FeedbackForm
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.columns
+  height: 70%
+  display: flex
+  align-items: center
+</style>
